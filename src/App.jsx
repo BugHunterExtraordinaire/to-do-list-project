0746457ffec122e5014a02ddd8bdab94f052c0c8
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-02-11 18:12:17 +0200

    Finalize input layout styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,29 @@ export default function App() {
     const savedTasks = localStorage.getItem("tasks");
     return savedTasks ? JSON.parse(savedTasks) : [];
   }));
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <h1>Task Managment Dashboard</h1>
-      <TaskInput tasks={tasks} setTasks={setTasks} />
-      <TaskList tasks={tasks} setTasks={setTasks} />
+      <TaskInput tasks={tasks} setTasks={setTasks} windowWidth={windowWidth} />
+      <TaskList tasks={tasks} setTasks={setTasks} windowWidth={windowWidth} />
     </>
   )
 }
